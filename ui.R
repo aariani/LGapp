d2eab65f8a78f948367e68548a1925feb3412009
aariani <aariani@ucdavis.edu>
2016-08-22 15:51:52 -0700

    Started UI development

---
+++ ui.R
@@ -0,0 +1,38 @@
+## ui.R
+
+shinyUI(navbarPage('LGapp',
+	tabPanel('Data Conversion',
+		sidebarPanel(
+			h2('Convert VCF files'),
+			tags$hr(),
+			fileInput('vcf', 'Choose VCF file'),
+			tags$hr(),
+			selectInput('outformat',
+				'Select Output Format',
+				choices=c('lfmm', 'geno'),
+				)
+			)
+		),
+	navbarMenu('Bioclimatic Data',
+		tabPanel('Download',
+		sidebarPanel(
+			h2('Download bioclimatic data'),
+			tags$hr(),
+			selectInput('climdata',
+				'Select Climatic Data', 
+				choices=c('All',
+					'BIO1 = Annual Mean Temperature',
+					'BIO2 = Mean Diurnal Range')
+				),
+			tags$hr(),
+			p('Data downloaded from the ', a('WorldClim database', href='http://www.worldclim.org/'))
+				)
+			),
+		tabPanel('PCA',
+			sidebarPanel(
+				h2('PCA on bioclimatic variables')
+				)
+			)
+		)
+	)
+)
